39047505c1c093fade486ffb2f45438d835d9d19
MatheusEduardoPy <matheuslopez.net@gmail.com>
2024-06-01 10:30:23 -0300

    Criei o app blog, modifiquei algumas coisinhas '-'

--- project/settings.py
+++ project/settings.py
@@ -21,15 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY','change-me')
+SECRET_KEY = "vn20uzx$-vsy#+5#uaf&!x@n91($)ot0cfyf0bkcg*@r414xgy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.getenv('DEBUG',0)))
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',')
-    if h.strip()
-]
+ALLOWED_HOSTS: list[str]= []
 
 
 # Application definition
@@ -120,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = (
+    BASE_DIR / 'blog' /'base_static' ,
+    )
+STATIC_ROOT = BASE_DIR / 'blog' /'static' #collectstatic
+
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
