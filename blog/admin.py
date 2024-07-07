b59851bed2c058be218cd8971d9c9f5c563c76b3
MatheusEduardoPy <matheuslopez.net@gmail.com>
2024-07-07 19:15:10 -0300

    Summernote ao post e page + link do post no posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+
 from blog.models import Tag, Category, Page, Post
+
+from django_summernote.admin import SummernoteModelAdmin
+
+from django.utils.safestring import mark_safe
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -24,7 +30,8 @@ class CategoryAdmin(admin.ModelAdmin):
     }
     
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published'
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'content'
@@ -37,7 +44,8 @@ class PageAdmin(admin.ModelAdmin):
     }
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id','title','is_published', 'created_by',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
@@ -45,7 +53,9 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = 'category', 'is_published',
     list_editable = 'is_published',
     ordering = '-id',
-    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    readonly_fields = (
+        'created_at', 'updated_at', 'created_by', 'updated_by', 
+        'link',)
     prepopulated_fields = {
         "slug":('title',),
     }
@@ -58,3 +68,13 @@ class PostAdmin(admin.ModelAdmin):
             obj.created_by = request.user # type: ignore
 
         obj.save()
+
+    def link(self, obj):
+        if not obj.pk:
+            return '-'
+        
+        url_do_post = obj.get_absolute_url()
+
+        safe_link = mark_safe(f'<a target="_blank" href="{url_do_post}">Ver post</a>')
+
+        return safe_link
