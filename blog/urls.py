2fbe9848207968a98ae6ba06b250ffeb46e1958f
MatheusEduardoPy <matheuslopez.net@gmail.com>
2024-07-12 10:31:42 -0300

    Mudando para CBV urls category, tag, search e page

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from blog.views import PostListView, post, page, CreatedByListView, category, tag, search
+from blog.views import PostListView, post, PageDetailView, CreatedByListView, CategoryListView, TagListView, SearchListView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug>/', page, name='page'),
+    path('page/<slug:slug>/', PageDetailView.as_view(), name='page'),
     path(
         'created_by/<int:author_id>/', 
         CreatedByListView.as_view(), 
         name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
-    path('tag/<slug:slug>/', tag, name='tag'),
-    path('search/', search, name='search'),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
+    path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
+    path('search/', SearchListView.as_view(), name='search'),
 ]
